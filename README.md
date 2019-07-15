af257cb3f0b3fe09d79335565d939763e93cfc6e
佐藤昭文 <>
2019-07-15 16:57:48 +0900

    debug process

---
+++ README.md
@@ -0,0 +1,5 @@
+## command
+
+### dev
+elm-live src/Main.elm -- --debug --output=build/elm.js
+
--- src/Main.elm
+++ src/Main.elm
@@ -1,79 +1,90 @@
-module Main exposing (Model, Msg(..), Todo, init, main)
-
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (..)
+import Task
+import Time
 
 
-main : Program () Model Msg
+
+-- MAIN
+
+
 main =
-    Browser.sandbox
-        { init = init
-        , update = update
-        , view = view
-        }
-
-
-
--- Model
-
-
-type alias Todo =
-    { title : String
-    , description : String
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
     }
 
 
+
+-- MODEL
+
+
 type alias Model =
-    { todos : List Todo }
+  { zone : Time.Zone
+  , time : Time.Posix
+  }
 
 
-init : Model
-init =
-    { todos = [] }
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model Time.utc (Time.millisToPosix 0)
+  , Task.perform AdjustTimeZone Time.here
+  )
 
 
 
--- Update
+-- UPDATE
 
 
 type Msg
-    = Add
+  = Tick Time.Posix
+  | AdjustTimeZone Time.Zone
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        Add ->
-            { model | todos = Todo "Todoタイトル" "Todo詳細" :: model.todos }
+  case msg of
+    Tick newTime ->
+      ( { model | time = newTime }
+      , Cmd.none
+      )
+
+    AdjustTimeZone newZone ->
+      ( { model | zone = newZone }
+      , Cmd.none
+      )
 
 
 
--- View
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 1000 Tick
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ viewTable model.todos
-        , button [ onClick Add ] [ text "Add" ]
-        ]
-
-
-viewTable : List Todo -> Html Msg
-viewTable todos =
-    table []
-        [ thead []
-            [ th [] [ text "Title" ]
-            , th [] [ text "Description" ]
-            ]
-        , tbody [] (List.map viewTr todos)
-        ]
-
-
-viewTr : Todo -> Html Msg
-viewTr todo =
-    tr []
-        [ td [] [ text todo.title ]
-        , td [] [ text todo.description ]
-        ]
+  let
+    hour   = String.fromInt (Time.toHour   model.zone model.time)
+    minute = String.fromInt (Time.toMinute model.zone model.time)
+    second = String.fromInt (Time.toSecond model.zone model.time)
+  in
+  div
+    [ style "background" "#7F7FD5"
+      ,style "background" "-webkit-linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5)"
+      ,style "background" "linear-gradient(to right, #91EAE4, #86A8E7, #7F7FD5)"
+    ]
+    [
+      h1 [] [ text "Nxt" ]
+      ,p [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+      ,p [] [ text "hello, world" ]
+    ]
